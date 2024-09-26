b1233f70b3307b2fccd0669f97d791ddf922d6df
VexVisualz <drnebel81@gmail.com>
2024-09-26 22:06:40 +0200

    Main test changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(BluddyRandom.randString(8, false, false, false, true));
+
+        String rands = BluddyRandom.randString(50);
+
+        for (int i = 0; i < rands.length(); i++) {
+            System.out.print(rands.toCharArray()[i]);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
